6e91d41e215dc1b8bcbf43f0b8a6beb350039d36
Niklas Rother <info@niklas-rother.de>
2015-02-03 19:54:18 +0100

    Yay, no octal constants in C#
    Windows Calculator to the rescue!

--- LowLevel/Inode.cs
+++ LowLevel/Inode.cs
@@ -10,26 +10,26 @@ namespace UnixV6FsTools.LowLevel
     [Flags]
     enum Permissions
     {
-        SetUserId = 04000,
-        SetGroupId = 02000,
-        UserRead = 0400,
-        UserWrite = 0200,
-        UserExecute = 0100,
-        GroupRead = 0040,
-        GroupWrite = 0020,
-        GroupExecute = 0010,
-        OthersRead = 0004,
-        OthersWrite = 0002,
-        OthersExecute = 0001,
+        SetUserId = 0x800, //04000
+        SetGroupId = 0x400, //02000
+        UserRead = 0x100, //0400
+        UserWrite = 0x80, //0200
+        UserExecute = 0x40, //0100
+        GroupRead = 0x20, //0040
+        GroupWrite = 0x10, //0020
+        GroupExecute = 0x8, //0010
+        OthersRead = 0x4, //0004
+        OthersWrite = 0x2, //0002
+        OthersExecute = 0x1, //0001
     }
 
     enum FileType
     {
         //please note that these types overlap and are not really "flags" in that sense
-        Plain = 0,
-        Directory = 040000,
-        CharSpecial = 020000,
-        BlockSpecial = 060000,
+        Plain = 0x0, //0
+        Directory = 0x4000, //040000
+        CharSpecial = 0x2000, //020000
+        BlockSpecial = 0x6000, //060000
     }
     
     class Inode
@@ -37,9 +37,9 @@ namespace UnixV6FsTools.LowLevel
         public const int INODE_SIZE = 32; //32byte per Inode
         public const int BLOCK_COUNT = 8; //8 direct blocks per Inode
         public const int INDIRECT_BLOCK_COUNT = FileSystem.BLOCK_SIZE / 2; //16bit per indirect block
-        
-        private const int LARGE_FLAG = 010000;
-        private const int ALLOCATED_FLAG = 0100000;
+
+        private const int LARGE_FLAG = 0x1000; //010000
+        private const int ALLOCATED_FLAG = 0x8000; //0100000
 
         public bool IsAllocated { get; set; }
         public bool IsLarge { get; set; }
@@ -65,8 +65,8 @@ namespace UnixV6FsTools.LowLevel
             int mode = stream.ReadInt16(); //mode
 
             inode.IsAllocated = (mode & ALLOCATED_FLAG) != 0;
-            inode.Permissions = (Permissions)(mode & 0777); //mask out non-permission bits
-            inode.FileType = (FileType)(mode & 060000); //s.a.
+            inode.Permissions = (Permissions)(mode & 0x1FF /*0777*/); //mask out non-permission bits
+            inode.FileType = (FileType)(mode & 0x6000 /*060000*/); //s.a.
             inode.IsLarge = (mode & LARGE_FLAG) != 0;
             inode.LinkCount = stream.ReadByte(); //nlink
             inode.UserId = stream.ReadByte(); //uid
